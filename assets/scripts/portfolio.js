80f9c32d1bcf2b38d14e2f0f5ad3eeb0799d7be6
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2016-03-11 22:53:34 -0500

    Add scrollspy, scrolling to sections, remove vertical carousel

---
+++ assets/scripts/portfolio.js
@@ -0,0 +1,23 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+	var offsetFromTop = $(".navbar").offset().top;
+
+	if (offsetFromTop > 50) { // scrolled 50px away from top
+		$(".navbar-fixed-top").addClass("top-nav-collapse");
+		$(".navbar-fixed-top").removeClass("navbar-transparent");
+	} else { // within 50px from top
+		$(".navbar-fixed-top").removeClass("top-nav-collapse");
+		$(".navbar-fixed-top").addClass("navbar-transparent");
+	}
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
