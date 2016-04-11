8ae9ad4cca0b17d684cbbd3b921e1024e80be16a
Ajayveer Singh Aujla <ajay.aujla@hotmail.com>
2016-04-11 03:59:37 -0400

    Cleanup files

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -16,11 +16,11 @@ $(document).ready(function() {
 
 		onLeave: function(index, nextIndex, direction) {
 			if(nextIndex == 1) { // going to first page
-				$(".navbar-fixed-top").removeClass("top-nav-collapse");
-				$(".navbar-fixed-top").addClass("navbar-transparent");
+				$('.navbar-fixed-top').removeClass('top-nav-collapse');
+				$('.navbar-fixed-top').addClass('navbar-transparent');
 			} else {
-				$(".navbar-fixed-top").addClass("top-nav-collapse");
-				$(".navbar-fixed-top").removeClass("navbar-transparent");
+				$('.navbar-fixed-top').addClass('top-nav-collapse');
+				$('.navbar-fixed-top').removeClass('navbar-transparent');
 			}
 		},
 
